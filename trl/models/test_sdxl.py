285977c6f6248525aace247c887c62871ccc9aa5
karun <mailto.karun.py@gmail.com>
2024-12-11 22:03:08 -0500

    Add SDXL support

---
+++ trl/models/test_sdxl.py
@@ -0,0 +1,11 @@
+from modeling_sd_xl import StableDiffusionXLPipeline
+import torch
+
+pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+# pipe.to("cuda")
+
+prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+
+image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+image.save("baby_racoon.png")
+
